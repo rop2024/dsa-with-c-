58d7e7a9b0d735e6e1198cdb0770c824b4a359f8
rop2024 <rohitpandey231456@gmail.com>
2024-12-31 10:20:40 +0530

    SortCheck completed

--- _15_array_complex/05_sortCheck.cpp
+++ _15_array_complex/05_sortCheck.cpp
@@ -5,6 +5,11 @@ bool sort_check(int arary[], int size);
 
 int main()
 {
+    int array[] = {1,2,3,4,5};
+    int array2[] = {1,0,4};
+
+    cout << "For array 1 : " << sort_check(array, 5) << endl;
+    cout << "For array 2 : " << sort_check(array2, 3) << endl;
     return 0;
 }
 
