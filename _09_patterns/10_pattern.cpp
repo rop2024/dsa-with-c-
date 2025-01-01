94e8d046dca14f7a78dc6f28f29ba5c7d77ce656
rop2024 <rohitpandey231456@gmail.com>
2025-01-01 10:36:57 +0530

    pattern 10 completed

--- _09_patterns/10_pattern.cpp
+++ _09_patterns/10_pattern.cpp
@@ -11,7 +11,50 @@ using namespace std;
 
 int main()
 {
+    int height = 0;
+    do
+    {
+        cout << "Enter height : ";
+        cin >> height;
+
+        if(height <= 0 || height >=9)
+        {
+            cout << "Height should be above 0 and below 9" << endl;
+        }
+        else
+        {
+            break;
+        }
+    } while (true);
     
-    
+    int height_utl = height - 1;
+
+    // downward ladder
+    for(int i = 1; i <= height_utl; i++)
+    {
+        for(int stars = 1; stars <= i; stars++)
+        {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    // middle lane
+    for(int i = 1; i <= height; i++)
+    {
+        cout << '*';
+    }
+    cout << endl;
+
+    // opposite downward stairs
+    for(int i = height_utl; i >= 1; i--)
+    {
+        for(int stars = 1; stars <= i; stars++)
+        {
+            cout << '*';
+        }
+        cout << endl;
+    }
+
     return 0;
 }
