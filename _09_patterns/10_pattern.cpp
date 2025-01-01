11173b2f788b795226194764a140440ac0620fcd
rop2024 <rohitpandey231456@gmail.com>
2025-01-01 10:26:21 +0530

    changes in deleting wrong solution

--- _09_patterns/10_pattern.cpp
+++ _09_patterns/10_pattern.cpp
@@ -11,32 +11,7 @@ using namespace std;
 
 int main()
 {
-    int height = 3;
-
-    for(int i = 1; i < height; i++)
-    {
-        for(int stars = 1; stars <= i; stars++)
-        {
-            cout << '*';
-        }
-        cout << endl;
-    }
-
-    for(int i = 1; i < height+1; i++)
-    {
-        cout << '*';
-    }
-    cout << endl;
-
-    for(int i = height; i > 0; i++)
-    {
-        for(int stars = 1; stars <= i; stars++)
-        {
-            cout << '*';
-        }
-        cout << endl;
-    }
-    cout << endl;
+    
     
     return 0;
 }
